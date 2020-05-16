5cbf7074e56f2bb1926a2b305204bed8a548e43e
yinlong22 <342241857@.com>
2020-05-17 00:59:31 +0800

    修复二维码不出现的 bug 将图片换成链接 从掘金搬到GitHub

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ window.onload = function () {
 if (document.documentElement.clientWidth > 500) {
     window.alert('请使用手机打开，以保证浏览效果')
     const img = document.createElement('img')
-    img.src = 'https://user-gold-cdn.xitu.io/2020/5/13/1720d99b435380e3?w=256&h=256&f=png&s=6624'
+    img.src = 'https://camo.githubusercontent.com/8eddbb4749dccef8925ab56049892842ba07c1ea/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f31332f313732306439396234333533383065333f773d32353626683d32353626663d706e6726733d36363234'
     img.style.position = 'fixed'
     img.style.top = '26%'
     img.style.left = '50%'
